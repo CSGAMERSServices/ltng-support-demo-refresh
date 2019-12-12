cc89aae16d84c6dc486441cae5ed21f949746373
Paul Roth <paulroth3d@gmail.com>
2019-12-12 10:17:11 -0600

    UPDATE: make the header a bit more friendly and complete

--- dx/force-app/main/default/lwc/ltng_DynamicHeader/ltng_DynamicHeader.js
+++ dx/force-app/main/default/lwc/ltng_DynamicHeader/ltng_DynamicHeader.js
@@ -6,14 +6,52 @@
 import { LightningElement, track, api } from 'lwc'; //eslint-disable-line no-unused-vars
 
 export default class ltng_DynamicHeader extends LightningElement {
+  /**
+   * The group of the icon (ex: standard, utility, etc)
+   * @type {string}
+   */
   @api iconGroup;
+
+  /**
+   * The name of the icon (ex:account or search)
+   * @see https://lightningdesignsystem.com/icons/
+   * @type {string}
+   */
   @api iconName;
+
+  /**
+   * The alternative text for screen readers for the icon.
+   * @type {string}
+   */
   @api iconAlternativeText;
+
+  /**
+   * The title of the box
+   * @type {string}
+   */
   @api title;
+
+  /**
+   * The detail text to show beneath the title
+   * @type {string}
+   */
   @api detail;
 
+  /**
+   * Determines the icon phrase used in many areas within LWS
+   * (ex: 'standard:opportunity' or 'utility:search')
+   * @type {string}
+   */
   @api
   get iconPhrase() {
     return this.iconGroup + ':' + this.iconName;
+  }
+
+  /**
+   * Whether the detail information has been provided (and should be shown)
+   * @type {boolean}
+   */
+  @api get hasDetail() {
+    return this.detail ? true : false;
   }
 }
