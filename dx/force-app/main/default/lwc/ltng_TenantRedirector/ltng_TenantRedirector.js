f9bdd4240e307e254204cfc54c1000cfe44d25ef
Paul Roth <paulroth3d@gmail.com>
2019-12-12 10:42:09 -0600

    UPDATE: use the wire service to get the tenant information

--- dx/force-app/main/default/lwc/ltng_TenantRedirector/ltng_TenantRedirector.js
+++ dx/force-app/main/default/lwc/ltng_TenantRedirector/ltng_TenantRedirector.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+/* eslint-disable no-console */
+/**
+ * Component that shows information about the tenant
+ * and allows the user to edit
+ **/
+import { LightningElement, track, api, wire } from 'lwc';
 
-export default class ltng_TenantRedirector extends LightningElement {}
+import {getRecord} from 'lightning/uiRecordApi';
+import TENANT_FIELD from '@salesforce/schema/ltng_TenantChild__c.Tenant__c';
+
+export default class ltng_TenantRedirector extends LightningElement {
+  /** use this to get the id of the current record we are on */
+  @api recordId;
+  
+  /** get the id of the tenant */
+  @wire(getRecord, {recordId: '$recordId', fields: [TENANT_FIELD]})
+  record;
+
+  /** id of the tenant this child is associated with */
+  get tenantId() {
+    return (!this.record || !this.record.data) ? null : this.record.data.fields.Tenant__c.value;
+  }
+
+  constructor() {
+    super();
+    console.log('Redirector completed');
+  }
+}
